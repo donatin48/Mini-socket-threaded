5d94ae3be016cba8457075632931f3bf7339df2b
donatin48 <40335400+donatin48@users.noreply.github.com>
2021-12-27 03:24:56 +0100

    update to add progress bar

--- client.py
+++ client.py
@@ -1,5 +1,11 @@
 import socket
+import tkinter as tk
+from tkinter import filedialog
+import os
+from tqdm import tqdm
 
+SEPARATOR = "<SEPARATOR>"
+CHUNK_SIZE = 4096
 ip = ""
 port = 5000
 if not ip:
@@ -13,24 +19,57 @@ class Client:
         self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.s.connect((self.host, self.port))
 
-    def receive(self,extension):
-        with open(f"received_file{extension}", "wb") as file:
+    def choice_path(self) -> str:
+        root = tk.Tk()
+        root.withdraw()
+        return filedialog.asksaveasfilename()
+
+
+    def receive(self):
+        # receive the filename and file size from the server
+        temp = self.s.recv(CHUNK_SIZE).decode()
+        filename, filesize = temp.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        filesize = int(filesize)
+        # print the information in gigabytes
+        print(f"Receiving {filename} of size {round(filesize/1024/1024,2)} MB\n")
+        if len(filename) > 25: # if the filename is too long reduce it
+            filename_print = filename[:25] + "..."
+        else:
+            filename_print = filename
+        # start receiving the file from the server
+        reponse = str(input("Do you want to change the name of the file? (y/n)\n"))
+        if reponse == "y":
+            path = self.choice_path()
+            if not path :
+                print("No file selected.")
+                input()
+                exit()
+        else:
+            path = os.path.join(os.getcwd(), filename)
+        print("\n")
+        progression = tqdm(range(filesize), f"Receiving {filename_print}", unit="B", unit_scale=True, unit_divisor=1024)
+        
+        with open(path, "wb") as file:
             while True:
                 data = self.s.recv(4096)
                 # print(f"data: \n {data}")
                 if not data:
                     break
                 file.write(data)
+                progression.update(len(data))
         
 
 print("Connecting to server...")
 client = Client(ip, port)
 print("Connection established.")
-extension = str(input("Enter file extension: "))
-if extension[0] != ".":
-    extension = "." + extension
-client.receive(extension)
-print("Successfully received file.")
+
+try :
+    client.receive()
+    print("Successfully received file.")
+except Exception as e:
+    print(f"Connection lost: {e}")
+
 input()
 
 
--- server.py
+++ server.py
@@ -1,12 +1,17 @@
-from os import path
+from re import A
 import socket
 import threading
 import tkinter as tk
 from tkinter import filedialog
 from threading import Thread
+import os
+from tqdm import tqdm
+
 
 port = 5000
 host = ""
+SEPARATOR = "<SEPARATOR>"
+CHUNK_SIZE = 4096
 
 # get the file path
 def get_file_path():
@@ -22,24 +27,51 @@ class Server(Thread):
         self.c = sock
 
 
-    def send(self, data):
-        with open(data, "rb") as file:
-            while True:
-                l = file.read(4096)
-                while (l):
-                    self.c.send(l)
-                    l = file.read(4096)
-                if not l:
-                    self.c.close()
-                    break  
-        threads.remove(self)   
-        print(f"File has been sent. thread count: {len(threads)}\n")
-        self.c.close()
+    def send(self, path):
+        try : 
+            # send the filename and file size to the client
+            filename = path.split("/")[-1]
+            filesize = os.path.getsize(path)
+            self.c.send(f"{filename}{SEPARATOR}{filesize}".encode())
+            if len(filename) > 25: # if the filename is too long reduce it
+                filename_print = filename[:25] + "..."
+            else:
+                filename_print = filename
+            # if   
+            progression = tqdm(range(filesize), f"Sending {filename_print}", unit="B", unit_scale=True, unit_divisor=1024)
+            # print("\n")
+            with open(path, "rb") as file:
+                while True:
+                    
+                    l = file.read(CHUNK_SIZE)
+                    while (l):
+                        self.c.send(l)
+                        l = file.read(CHUNK_SIZE)
+                        progression.update(len(l))
+                    if not l:
+                        self.c.close()
+                        threads.remove(self)   
+                        progression.close()
+                        break  
+                    
+            self.c.close()
+            print(f"File has been sent. thread count: {len(threads)}\n")
+        except Exception as e:
+            print(f"\nError in thread n°{threads.index(self)}:  {e}")
+            self.c.close()
+            threads.remove(self)
+            progression.close()
+            print(f"Threads count: {len(threads)}\n")
+            
 
 tcpsock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 tcpsock.bind((host, port))
 
 path = get_file_path()
+if not path:
+    print("No file selected.")
+    input()
+    exit()
 print("You Send: " + path)
 threads = []
 while True:
@@ -53,4 +85,6 @@ while True:
         threads.append(thread)
         print(f"Number of active threads: {len(threads)}")
     except Exception as e:
+        if e.errno == 10054:
+            print("Connection lost with client.")
         print(f"Connection lost: {e}")
